ff371f3c0670401c437e49edb47644300e36fd20
Marin Becker <marin.becker@yahoo.fr>
2023-11-25 16:33:25 +0100

    return value pb solved. need read/write check libft/ft_to_base.c

--- 2.0-utils/main.c
+++ 2.0-utils/main.c
@@ -4,44 +4,114 @@
 
 int main() {
     char c = 'A';
-    char *s = "Hello, world!";
+    char *s = NULL;
     int d = 12345;
     unsigned int u = 54321;
     int negative = -6789;
     unsigned int hex = 0xABCD;
     void *ptr = &d;
 
-	ft_printf("%X\n", 0);
+	int ogRes;
+	int ftRes;
 
-
-	 printf("\n   printf %%c: %c\n", c);
-	ft_printf("ft_printf %%c: %c\n", c);
-	 printf("\n   printf %%s: %s\n", s);
-	ft_printf("ft_printf %%s: %s\n", s);
-	 printf("\n   printf %%p: %p\n", ptr);
-	ft_printf("ft_printf %%p: %p\n", ptr);
-	 printf("\n   printf %%d: %d\n", d);
-	ft_printf("ft_printf %%d: %d\n", d);
-	 printf("\n   printf %%i: %i\n", d);
-	ft_printf("ft_printf %%i: %i\n", d);
-	 printf("\n   printf %%u: %u\n", u);
-	ft_printf("ft_printf %%u: %u\n", u);
-	 printf("\n   printf %%x: %x\n", hex);
-	ft_printf("ft_printf %%x: %x\n", hex);
-	 printf("\n   printf %%X: %X\n", hex);
-	ft_printf("ft_printf %%X: %X\n", hex);
-	 printf("\n   printf %%d with negative: %d\n", negative);
-	ft_printf("ft_printf %%d with negative: %d\n", negative);
-	 printf("\n   printf %%i with negative: %i\n", negative);
-	ft_printf("ft_printf %%i with negative: %i\n", negative);
-	 printf("\n   printf %%p with NULL pointer: %p\n", NULL);
-	ft_printf("ft_printf %%p with NULL pointer: %p\n", NULL);
-	 printf("\n   printf %%x with zero: %x\n", 0);
-	ft_printf("ft_printf %%x with zero: %x\n", 0);
-	 printf("\n   printf %%X with zero: %X\n", 0);
-	ft_printf("ft_printf %%X with zero: %X\n", 0);
-	 printf("\n   printf %%%%: %%\n");
-	ft_printf("ft_printf %%%%: %%\n");
-    return 0;
+	ogRes =  printf("og %%c: %c\n", c);
+	ftRes = ft_printf("ft %%c: %c\n", c);
+	red();
+	printf("og = %d\nft = %d\n\n", ogRes, ftRes);
+	reset();
+	
+	ogRes =  printf("og %%s: %s\n", s);
+	ftRes = ft_printf("ft %%s: %s\n", s);
+	red();
+	printf("og = %d\nft = %d\n\n", ogRes, ftRes);
+	reset();
+	
+	ogRes =  printf("og %%p: %p\n", ptr);
+	ftRes = ft_printf("ft %%p: %p\n", ptr);
+	red();
+	printf("og = %d\nft = %d\n\n", ogRes, ftRes);
+	reset();
+	
+	ogRes =  printf("og %%d: %d\n", d);
+	ftRes = ft_printf("ft %%d: %d\n", d);
+	red();
+	printf("og = %d\nft = %d\n\n", ogRes, ftRes);
+	reset();
+	
+	ogRes =  printf("og %%i: %i\n", d);
+	ftRes = ft_printf("ft %%i: %i\n", d);
+	red();
+	printf("og = %d\nft = %d\n\n", ogRes, ftRes);
+	reset();
+	
+	ogRes =  printf("og %%u: %u\n", u);
+	ftRes = ft_printf("ft %%u: %u\n", u);
+	red();
+	printf("og = %d\nft = %d\n\n", ogRes, ftRes);
+	reset();
+	
+	ogRes =  printf("og %%x: %x\n", hex);
+	ftRes = ft_printf("ft %%x: %x\n", hex);
+	red();
+	printf("og = %d\nft = %d\n\n", ogRes, ftRes);
+	reset();
+	
+	ogRes =  printf("og %%X: %X\n", hex);
+	ftRes = ft_printf("ft %%X: %X\n", hex);
+	red();
+	printf("og = %d\nft = %d\n\n", ogRes, ftRes);
+	reset();
+	
+	ogRes =  printf("og %%d with negative: %d\n", negative);
+	ftRes = ft_printf("ft %%d with negative: %d\n", negative);
+	red();
+	printf("og = %d\nft = %d\n\n", ogRes, ftRes);
+	reset();
+	
+	ogRes =  printf("og %%i with negative: %i\n", negative);
+	ftRes = ft_printf("ft %%i with negative: %i\n", negative);
+	red();
+	printf("og = %d\nft = %d\n\n", ogRes, ftRes);
+	reset();
+	
+	ogRes =  printf("og %%p with NULL pointer: %p\n", NULL);
+	ftRes = ft_printf("ft %%p with NULL pointer: %p\n", NULL);
+	red();
+	printf("og = %d\nft = %d\n\n", ogRes, ftRes);
+	reset();
+	
+	ogRes =  printf("og %%x with zero: %x\n", 0);
+	ftRes = ft_printf("ft %%x with zero: %x\n", 0);
+	red();
+	printf("og = %d\nft = %d\n\n", ogRes, ftRes);
+	reset();
+	
+	ogRes =  printf("og %%X with zero: %X\n", 0);
+	ftRes = ft_printf("ft %%X with zero: %X\n", 0);
+	red();
+	printf("og = %d\nft = %d\n\n", ogRes, ftRes);
+	reset();
+	
+	ogRes =  printf("og %%%%: %%\n");
+	ftRes = ft_printf("ft %%%%: %%\n");
+    red();
+	printf("og = %d\nft = %d\n\n", ogRes, ftRes);
+	reset();
+	
+	return 0;
 }
 
+
+/*#include <stdio.h>
+
+int	main()
+{
+	int ogRes;
+	int ftRes;
+
+	ogRes =  printf("og %%c: %c\n", 'c');
+	ftRes = ft_printf("ft %%c: %c\n", 'c');
+	red();
+	printf("og = %d\nft = %d\n\n", ogRes, ftRes);
+	reset();
+}*/
