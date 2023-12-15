30f75b6d1eaff4b6ac564a4842b04b3e5b821ab7
marinsucks <marin.becker@yahoo.fr>
2023-12-15 13:02:31 +0100

    git.sh

--- git.sh
+++ git.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+GREY='\033[1;30m'
+LGREY='\033[0;30m'
 RED='\033[1;31m'
 LRED='\033[0;31m'
 GREEN='\033[1;32m'
 LGREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 LYELLOW='\033[0;33m'
-GREY='\033[1;37m'
+BOLD='\033[1;37m'
 NC='\033[0m'
 SWAG='🫠'
 
@@ -34,23 +36,34 @@ git_action() {
     echo -e "\n${GREEN}REPO PUSH COMPLETED $SWAG${NC}"
 }
 
-setup_alias() {
-    echo -e "${LGREEN}Setting up 'push' alias in ~/.bashrc and ~/.zshrc...${NC}"
-    echo -e "push() {\n\t$PWD/git.sh \"\$@\"\n} " >> ~/.bashrc
-    source ~/.bashrc
-	echo -e "push() {\n\t$PWD/git.sh \"\$@\"\n} " >> ~/.zshrc
-    echo -e "${GREEN}Alias 'push' set up successfully.${NC}"
+setpush() {
+    echo -e "${LGREEN}Setting up 'push' alias...${NC}"
+    if [ "$(echo $SHELL)" = "/bin/bash" ]; then
+        echo -e "alias push=\"$PWD/git.sh\" \"\$@\"" >> ~/.bashrc
+        source ~/.bashrc
+        echo -e "${GREEN}Alias 'push' set up successfully in ~/.bashrc.${NC}"
+    elif [ "$(echo $SHELL)" = "/bin/zsh" ]; then
+        echo -e "alias push=\"$PWD/git.sh\" \"\$@\"" >> ~/.zshrc
+        source ~/.zshrc
+        echo -e "${GREEN}Alias 'push' written successfully in ~/.zshrc.${NC}"
+        echo -e "${LYELLOW}If you use ohmyzsh, please run ${YELLOW}'source ~/.zshrc'${LYELLOW} to finish the installation.${NC}"
+    else
+        echo -e "${LRED}Could not set up alias 'push': please use ./git.sh instead.${NC}"
+        echo $0
+    fi
 }
 
 if [ $# -eq 0 ]; then
-    echo -e "${LRED}Missing commit message.${NC}"
+    echo -e "${LRED}Missing first argument.\n${LYELLOW}Try ./git.sh help${NC}"
     exit 1
 fi
 
 if [ "$1" == "setpush" ]; then
-	setup_alias
+	setpush
 elif [ "$1" == "help" ]; then
-	echo -e "Usage: push \"commit message\" \"[files to add]\""
+	
+    echo -e "${BOLD}USAGE\t\t${NC}push \"commit message\" \"[files to add]\""
+    echo -e "${BOLD}PUSH SETUP:\t${NC}./git.sh setpush  ${LGREY}-> aliases the push command, replacing ./git.sh${NC}"
 else
 	git_action "$@"
 fi
