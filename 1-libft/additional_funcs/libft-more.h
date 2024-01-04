7cfffff6d619f26b805d9bb2224745fb2bbca81a
Marin Becker <marin.becker@yahoo.fr>
2024-01-04 10:48:46 +0100

    moved additional functions to ./ and updated Makefile and header.

--- 1-libft/additional_funcs/libft-more.h
+++
@@ -1,6 +0,0 @@
-//int		ft_strcmp(const char *s1, const char *s2);
-//char	*ft_strstr(const char *haystack, const char *needle);
-//char	*ft_strcat(char *dest, const char *src);
-//char	*ft_strncat(char *s1, const char *s2, size_t n);
-//char	*ft_strncpy(char *dst, const char *src, size_t len);
-//char	*ft_strcpy(char *dst, const char *src);
