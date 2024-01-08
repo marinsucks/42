07dc06b181178cc8dd33d3bf6c8610b633320402
Marin Becker <marin.becker@yahoo.fr>
2024-01-08 18:24:57 +0100

    advancing.

--- 3.0-Push_swap/push_swap.c
+++ 3.0-Push_swap/push_swap.c
@@ -6,21 +6,13 @@
 /*   By: mbecker <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 15:10:25 by mbecker           #+#    #+#             */
-/*   Updated: 2024/01/08 15:24:31 by mbecker          ###   ########.fr       */
+/*   Updated: 2024/01/08 18:24:27 by mbecker          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
-
-# include "libft/libft.h"
-
-typedef	struct s_nums
+int main(int argc, char const *argv[])
 {
-	int				*num;
-	struct s_nums	*next;
-}					t_nums;
+	//take args as a list
+	return 0;
+}
 
-
-
-#endif
--- 3.0-Push_swap/push_swap.h
+++ 3.0-Push_swap/push_swap.h
@@ -6,8 +6,21 @@
 /*   By: mbecker <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 15:11:12 by mbecker           #+#    #+#             */
-/*   Updated: 2024/01/08 15:11:13 by mbecker          ###   ########.fr       */
+/*   Updated: 2024/01/08 18:22:11 by mbecker          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
 
+# include "libft/libft.h"
+
+typedef	struct s_nums
+{
+	int				*num;
+	struct s_nums	*next;
+}					t_nums;
+
+
+
+#endif
