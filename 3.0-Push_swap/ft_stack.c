10f9824bd2221e360c1908d8e893f0f6370762ec
marinsucks <marin.becker@yahoo.fr>
2024-01-10 00:28:23 +0100

    minor updates. check .txt file for macos report.

--- 3.0-Push_swap/ft_stack.c
+++ 3.0-Push_swap/ft_stack.c
@@ -1,20 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   ft_stack.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mbecker <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/08 15:11:12 by mbecker           #+#    #+#             */
-/*   Updated: 2024/01/09 23:54:25 by mbecker          ###   ########.fr       */
+/*   Created: 2024/01/09 23:16:45 by mbecker           #+#    #+#             */
+/*   Updated: 2024/01/09 23:54:27 by mbecker          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
+#include "push_swap.h"
 
-# include "libft/libft.h"
+//don't forget to free stack after use
+t_list	*ft_makestack(int argc, char const *argv[])
+{
+	t_list	*stack;
+	t_list	*new_node;
+	int		*numptr;
 
-t_list	*ft_makestack(int argc, char const *argv[]);
-
-#endif
+	stack = NULL;
+	while (argc > 1)
+	{
+		numptr = (int *)malloc(sizeof(int));
+		*numptr = ft_atoi(argv[--argc]);
+		new_node = ft_lstnew(numptr);
+		if (!new_node)
+			return (stack);
+		ft_lstadd_front(&stack, new_node);
+	}
+	return (stack);
+}
