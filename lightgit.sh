1c547a046b44eeda88abdc43edbaee2feee2f2c6
marinsucks <marin.becker@yahoo.fr>
2024-01-09 22:37:38 +0100

    command setup fixes

--- lightgit.sh
+++ lightgit.sh
@@ -94,7 +94,7 @@ function normalMode() {
 setlgit() {
     echo -e "${LGREEN}Setting up 'lgit' alias...${NC}"
     if [ "$(echo $SHELL)" = "/bin/bash" ]; then
-        echo -e "alias lgit=\"$PWD/git.sh\" \"\$@\"" >> ~/.bashrc
+        echo -e "alias lgit=\"$PWD/lightgit.sh\" \"\$@\"" >> ~/.bashrc
         source ~/.bashrc
         echo -e "${GREEN}Alias 'lgit' set up successfully in ~/.bashrc.${NC}"
     elif [ "$(echo $SHELL)" = "/bin/zsh" ]; then
@@ -103,7 +103,7 @@ setlgit() {
         echo -e "${BOLD}Alias 'lgit' written successfully in ~/.zshrc.${NC}"
         echo -e "If you use ohmyzsh, please run ${BOLD}'source ~/.zshrc'${NC} to finish the installation."
     else
-        echo -e "${LRED}Could not set up alias 'lgit': please use ./git.sh instead.${NC}"
+        echo -e "${LRED}Could not set up alias 'lgit': please use ./lightgit.sh instead.${NC}"
         echo $0
     fi
 }
@@ -112,7 +112,7 @@ help() {
 	echo -e "${BOLD}┌─────────────────────────────────────────────────────────────┐${NC}"
 	echo -e "${BOLD}│ LIGHTGIT.SH - a single command for git add, commit and push │${NC}"
 	echo -e "${BOLD}└─────────────────────────────────────────────────────────────┘${NC}\n"
-	echo -e "${BOLD}LGIT COMMAND SETUP:\t${NC}./git.sh setlgit  ${LGREY}-> creates the alias 'lgit' for './lightgit'${NC}\n"
+	echo -e "${BOLD}LGIT COMMAND SETUP:\t${NC}./lightgit.sh setlgit  ${LGREY}-> creates the alias 'lgit' for './lightgit'${NC}\n"
     echo -e "${BOLD}MODES:\t\t\t${NC}lgit n ${LGREY}-> normal mode, shows processes in detail.${NC}"
     echo -e "${BOLD}\t\t\t${NC}lgit q ${LGREY}-> quick mode, only necessary infos.${NC}\n"
 }
