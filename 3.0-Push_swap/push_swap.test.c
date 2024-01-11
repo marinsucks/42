3acf93cdfbd4b2a197741f9d4aa55b5e77c37f36
Marin Becker <marin.becker@yahoo.fr>
2024-01-11 18:20:46 +0100

    working.

--- 3.0-Push_swap/push_swap.test.c
+++ 3.0-Push_swap/push_swap.test.c
@@ -6,7 +6,7 @@
 /*   By: mbecker <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 13:54:27 by mbecker           #+#    #+#             */
-/*   Updated: 2024/01/10 16:27:50 by mbecker          ###   ########.fr       */
+/*   Updated: 2024/01/11 18:19:21 by mbecker          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,38 +20,40 @@ void	printList(t_list *list)
 		value = list->content;
 		ft_printf("%d\n", *value);
 		list = list->next;
-	}
+	}	
 }
 
-void	printBoth(t_list *list_a, t_list *list_b)
+void	printBoth(t_list **list_a, t_list **list_b)
 {
 	int *value_a;
 	int *value_b;
-	int len_a = ft_lstsize(list_a);
-	int len_b = ft_lstsize(list_b);
+	int len_a = ft_lstsize(*list_a);
+	int len_b = ft_lstsize(*list_b);
+	t_list *ptr_a = *list_a;
+	t_list *ptr_b = *list_b;
 
 	while (len_a > len_b)
 	{
-		value_a = list_a->content;
+		value_a = ptr_a->content;
 		ft_printf("%d\t\n", *value_a);
-		list_a = list_a->next;
+		ptr_a = ptr_a->next;
 		len_a--;
 	}
 	while (len_b > len_a)
 	{
-		value_b = list_b->content;
+		value_b = ptr_b->content;
 		ft_printf("\t%d\n", *value_b);
-		list_b = list_b->next;
+		ptr_b = ptr_b->next;
 		len_b--;
 	}
-	while (list_a || list_b)
+	while (ptr_a != NULL || ptr_b != NULL)
 	{
-		value_a = list_a->content;
-		value_b = list_b->content;
+		value_a = ptr_a->content;
+		value_b = ptr_b->content;
 		ft_printf("%d\t%d\n", *value_a, *value_b);
-		list_a = list_a->next;
-		list_b = list_b->next;
+		ptr_a = ptr_a->next;
+		ptr_b = ptr_b->next;
 	}
-	ft_printf("\033[1;37m_\t_\nA\tB\n\033[0m");
-	//ft_printf("%d\t%d\n", len_a, len_b);
+	ft_printf("\033[1;32m_\t_\nA\tB\n\033[0m");
+	ft_printf("\033[0;32m%d\t%d\n\n\033[0m", ft_lstsize(*list_a), ft_lstsize(*list_b));
 }
