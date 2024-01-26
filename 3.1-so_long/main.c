138e1949bf2fccc663b19ddf08ac94d5c527494e
Marin Becker <marin.becker@yahoo.fr>
2024-01-26 16:12:02 +0100

    getting minilbx in hand

--- 3.1-so_long/main.c
+++ 3.1-so_long/main.c
@@ -6,10 +6,19 @@
 /*   By: mbecker <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/26 11:01:32 by mbecker           #+#    #+#             */
-/*   Updated: 2024/01/26 14:17:57 by mbecker          ###   ########.fr       */
+/*   Updated: 2024/01/26 15:58:03 by mbecker          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
+int main(void)
+{
+    void	*mlx_ptr;
 
+    mlx_ptr = mlx_init();
+	if (!mlx_ptr)
+		return (1);
+    mlx_destroy_display(mlx_ptr);
+    free(mlx_ptr);
+}
--- 3.1-so_long/so_long.h
+++ 3.1-so_long/so_long.h
@@ -6,15 +6,20 @@
 /*   By: mbecker <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/26 11:01:23 by mbecker           #+#    #+#             */
-/*   Updated: 2024/01/26 14:19:06 by mbecker          ###   ########.fr       */
+/*   Updated: 2024/01/26 14:56:56 by mbecker          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-
-
 # include "libft/libft.h"
+
+# ifdef __unix__
+# include "minilibx-linux/mlx.h"
+# endif
+# ifdef __APPLE__
+# include "minilibx_opengl/mlx.h"
+# endif
 
 #endif
