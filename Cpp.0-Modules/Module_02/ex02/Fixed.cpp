84277937f9229e9d889508e096507f720456a3b4
Marin Becker <marin.becker@yahoo.fr>
2024-09-23 14:16:57 +0200

    uncommented code

--- Cpp.0-Modules/Module_02/ex02/Fixed.cpp
+++ Cpp.0-Modules/Module_02/ex02/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: mbecker <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/11 17:43:38 by mbecker           #+#    #+#             */
-/*   Updated: 2024/09/23 14:04:20 by mbecker          ###   ########.fr       */
+/*   Updated: 2024/09/23 14:16:40 by mbecker          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,28 +16,28 @@ const int Fixed::_fractionalBits = 8;
 
 Fixed::Fixed(): _val(0)
 {
-	//std::cout << "Default constructor called" << std::endl;
+	std::cout << "Default constructor called" << std::endl;
 }
 
 Fixed::Fixed(int arg): _val(arg * (1 << this->_fractionalBits))
 {
-	//std::cout << "Int constructor called" << std::endl;
+	std::cout << "Int constructor called" << std::endl;
 }
 
 Fixed::Fixed(float arg): _val(roundf(arg * (1 << this->_fractionalBits)))
 {
-	//std::cout << "Float constructor called" << std::endl;
+	std::cout << "Float constructor called" << std::endl;
 }
 
 Fixed::Fixed(const Fixed &other)
 {
-	//std::cout << "Copy constructor called" << std::endl;
+	std::cout << "Copy constructor called" << std::endl;
 	*this = other;
 }
 
 Fixed &Fixed::operator=(const Fixed &other)
 {
-	//std::cout << "Copy assignment operator called" << std::endl;
+	std::cout << "Copy assignment operator called" << std::endl;
 	if (this != &other)
 		this->_val = other._val;
 	return *this;
@@ -45,7 +45,7 @@ Fixed &Fixed::operator=(const Fixed &other)
 
 Fixed::~Fixed()
 {
-	//std::cout << "Destructor called" << std::endl;
+	std::cout << "Destructor called" << std::endl;
 }
 
 float	Fixed::toFloat(void) const
