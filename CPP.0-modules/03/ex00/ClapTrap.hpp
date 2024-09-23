53d72d705a91c6585ed27940b905195f9c359895
Marin Becker <marin.becker@yahoo.fr>
2024-09-23 16:51:21 +0200

    finished ex00

---
+++ CPP.0-modules/03/ex00/ClapTrap.hpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbecker <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/23 14:33:58 by mbecker           #+#    #+#             */
+/*   Updated: 2024/09/23 16:15:45 by mbecker          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <iostream>
+
+class ClapTrap
+{
+public:
+	ClapTrap();
+	ClapTrap(std::string name);
+	ClapTrap(const ClapTrap& other);
+	ClapTrap& operator=(const ClapTrap& other);
+	~ClapTrap();
+private:
+	std::string _name;
+	int _hitpoints;
+	int _energy_points;
+	int _attack_damage;
+public:
+	void attack(const std::string& target);
+	void takeDamage(unsigned int amount);
+	void beRepaired(unsigned int amount);
+private:
+	// Private member functions
+};
+
+# define ACTION_COST 1
+
+#endif
