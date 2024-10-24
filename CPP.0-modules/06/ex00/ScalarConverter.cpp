026d40a7e2d72183960251e73886bcb20a6ee739
Marin Becker <marin.becker@yahoo.fr>
2024-10-24 14:39:43 +0200

    finished scalarconverter and tested it

--- CPP.0-modules/06/ex00/ScalarConverter.cpp
+++ CPP.0-modules/06/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: mbecker <mbecker@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 17:08:11 by mbecker           #+#    #+#             */
-/*   Updated: 2024/10/23 18:01:37 by mbecker          ###   ########.fr       */
+/*   Updated: 2024/10/24 14:37:58 by mbecker          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,22 +32,24 @@ void	ScalarConverter::convert(const std::string& str)
 		std::cout << "'" << c << "'" << std::endl;
 	else
 		std::cout << "Non displayable" << std::endl;
+	
+	if ((!std::isdigit(c) && d == 0) 
+		&& str != "nan"
+		&& str != "inff" && str != "inf"  
+		&& str != "-inff" && str != "-inf")
+	{
+		std::cout << "int: " << static_cast<int>(c) << std::endl; 
+		std::cout << "float: "<< static_cast<float>(c) << "f" << std::endl; 
+		std::cout << "double: "<< static_cast<double>(c) << std::endl;
+		return ;
+	}
 
 	std::cout << "int: ";
-	if (i == atol(str.c_str()))
+	if (static_cast<double>(i) == d || (d >= i && d < i + 1))
 		std::cout << i << std::endl;
 	else
 		std::cout << "out of scope" << std::endl;
 
-	std::cout << "float: ";
-	if (str == "-inff" || str == "+inff" || str == "nanf")
-		std::cout << str << std::endl;
-	else
-		std::cout << f << "f" << std::endl;
-
-	std::cout << "double: ";
-	if (str == "-inf" || str == "+inf" || str == "nan")
-		std::cout << str << std::endl;
-	else
-		std::cout << d << std::endl;
+	std::cout << "float: " << f << "f" << std::endl;
+	std::cout << "double: " << d << std::endl;
 }
--- CPP.0-modules/06/ex00/main.cpp
+++ CPP.0-modules/06/ex00/main.cpp
@@ -6,24 +6,113 @@
 /*   By: mbecker <mbecker@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 17:25:04 by mbecker           #+#    #+#             */
-/*   Updated: 2024/10/22 17:54:32 by mbecker          ###   ########.fr       */
+/*   Updated: 2024/10/24 14:24:25 by mbecker          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 
+#include <iomanip>
+#include "colors.hpp"
+
+void	unit_test()
+{
+	std::string testint[] = {
+	    "0", "1", "42", "-42", "2147483647", "-2147483648", 
+	    "2147483648", "-2147483649"
+	};
+
+	for (size_t i = 0; i < sizeof(testint) / sizeof(testint[0]); i++)
+	{
+		std::cout << BLUE << testint[i] << NC << std::endl;
+		ScalarConverter::convert(testint[i]);
+		std::cout << std::endl;
+	}
+	std::cout << "---------------------------------\n" << std::endl;
+
+	std::string testfloat[] = {
+	    "0.0", "1.23", "-1.23", "42.0", "3.14159",
+	    "3.4028235e38", "-3.4028235e38"
+	};
+	
+	for (size_t i = 0; i < sizeof(testfloat) / sizeof(testfloat[0]); i++)
+	{
+		std::cout << BLUE << testfloat[i] << NC << std::endl;
+		ScalarConverter::convert(testfloat[i]);
+		std::cout << std::endl;
+	}
+	std::cout << "---------------------------------\n" << std::endl;
+
+	std::string testdouble[] = {
+		"1.7976931348623157e308", "-1.7976931348623157e308",
+	    "NaN", "INF", "-INF", 
+	    "1.8e308", "-1.8e308"
+	};
+
+	for (size_t i = 0; i < sizeof(testdouble) / sizeof(testdouble[0]); i++)
+	{
+		std::cout << BLUE << testdouble[i] << NC << std::endl;
+		ScalarConverter::convert(testdouble[i]);
+		std::cout << std::endl;
+	}
+	std::cout << "---------------------------------\n" << std::endl;
+
+	std::string testchar[] = {
+	    "a", "Z", "9", "!",
+	    "\0", "\n", "\t"
+	};
+
+	for (size_t i = 0; i < sizeof(testchar) / sizeof(testchar[0]); i++)
+	{
+		std::cout << BLUE << testchar[i] << NC << std::endl;
+		ScalarConverter::convert(testchar[i]);
+		std::cout << std::endl;
+	}
+	std::cout << "---------------------------------\n" << std::endl;
+
+	std::string testinvalid[] = {
+	    "abc", "42abc", "NaN", "4.2e4xyz", 
+	    "#", "@!", "12#34", 
+	    "", "   ", 
+	    "1.23.45", "12e34", "12.34.56"
+	};
+
+	for (size_t i = 0; i < sizeof(testinvalid) / sizeof(testinvalid[0]); i++)
+	{
+		std::cout << BLUE << testinvalid[i] << NC << std::endl;
+		ScalarConverter::convert(testinvalid[i]);
+		std::cout << std::endl;
+	}
+	std::cout << "---------------------------------\n" << std::endl;
+
+	std::string testspecial[] = {
+	    " 42", "42 ", " 42 ", 
+	    "00042", "0042.00"
+	};
+
+	for (size_t i = 0; i < sizeof(testspecial) / sizeof(testspecial[0]); i++)
+	{
+		std::cout << BLUE << testspecial[i] << NC << std::endl;
+		ScalarConverter::convert(testspecial[i]);
+		std::cout << std::endl;
+	}
+}
+
 int main()
 {
-
 	std::string input;
 	while (true) 
 	{
-		std::cout << "Enter a value, or exit: ";
+		std::cout << "Enter a value, 'unit' or 'exit': ";
 		if (!(std::cin >> input))
 			break;
 		if (input == "exit" || input == "EXIT" || input == "Exit" || input == "q")
 			break;
-		ScalarConverter::convert(input);
+		else if (input == "unit_test" || input == "unit")
+			unit_test();
+		else
+			ScalarConverter::convert(input);
 	}
 	return 0;
 }
+
