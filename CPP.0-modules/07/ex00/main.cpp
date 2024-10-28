fd2c4b9e8044730a1dc26ccba3cde467f7077825
Marin Becker <marin.becker@yahoo.fr>
2024-10-28 16:15:52 +0100

    finished ex00 for 07

---
+++ CPP.0-modules/07/ex00/main.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbecker <mbecker@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/28 16:04:24 by mbecker           #+#    #+#             */
+/*   Updated: 2024/10/28 16:15:00 by mbecker          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "whatever.hpp"
+
+int main()
+{
+	int a = 2;
+	int b = 3;
+
+	::swap(a, b);
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
+	std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
+
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min(c, d) = " << ::min(c, d) << std::endl;
+	std::cout << "max(c, d) = " << ::max(c, d) << std::endl;
+
+	return 0;
+}
---
+++ CPP.0-modules/07/ex00/whatever.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbecker <mbecker@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/28 15:54:58 by mbecker           #+#    #+#             */
+/*   Updated: 2024/10/28 16:06:16 by mbecker          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+template <typename T>
+void	swap(T& a, T& b)
+{
+	T tmp = a;
+	a = b;
+	b = tmp;
+}
+
+template <typename T>
+T	min(T& a, T& b)
+{
+	return (a >= b ? b : a);
+}
+
+template <typename T>
+T	max(T& a, T& b)
+{
+	return (a <= b ? b : a);
+}
