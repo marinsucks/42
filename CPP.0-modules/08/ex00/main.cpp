5250f7b3eb121723ad8299398f89f378a0eaf59e
Marin Becker <marin.becker@yahoo.fr>
2024-10-31 12:33:58 +0100

    added STL header file. program works with int containers without problem.

--- CPP.0-modules/08/ex00/main.cpp
+++ CPP.0-modules/08/ex00/main.cpp
@@ -6,75 +6,49 @@
 /*   By: mbecker <mbecker@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 16:53:57 by mbecker           #+#    #+#             */
-/*   Updated: 2024/10/30 17:58:12 by mbecker          ###   ########.fr       */
+/*   Updated: 2024/10/31 12:31:29 by mbecker          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "STL.hpp"
 #include "easyfind.hpp"
 
-#include <vector>
-#include <map>
-
-
-void vectorTest(void)
+template <class STL>
+void	test(std::string container)
 {
-	std::vector<int> v;
+	std::cout << container << ":" << std::endl;
+	
+	STL stl;
 	for (int i = 0; i < 10; i++)
-		v.push_back(i);
+		stl.insert(stl.begin(), i);
 
-	std::cout << "Vector contains: ";
-	for (std::vector<int>::iterator it = v.begin(); it != v.end(); it++)
+	std::cout << "Container contains: ";
+	for (typename STL::iterator it = stl.begin(); it != stl.end(); it++)
 		std::cout << *it << " ";
 	std::cout << std::endl;
 
 	try
 	{
-		std::cout << "Looking for 5 in the vector : ";
-		std::cout << easyfind(v, 5) << std::endl;
+		std::cout << "Looking for 5 in the container: ";
+		std::cout << easyfind(stl, 5) << std::endl;
 
-		std::cout << "Looking for 42 in the vector: ";
-		std::cout << easyfind(v, 42) << std::endl;
+		std::cout << "Looking for 42 in the container: ";
+		std::cout << easyfind(stl, 42) << std::endl;
 	}
 	catch (std::exception &e)
 	{
 		std::cout << e.what() << std::endl;
 	}
-}
-
-void mapTest(void)
-{
-	std::map<int, std::string> m;
-	m[1] = "one";
-	m[2] = "two";
-	m[3] = "three";
-
-	std::cout << "Map contains: ";
-	for (std::map<int, std::string>::iterator it = m.begin(); it != m.end(); it++)
-		std::cout << it->first << " : " << it->second << " ";
 	std::cout << std::endl;
-
-	try
-	{
-		std::cout << "Looking for 2 in the map : ";
-		std::cout << easyfind(m, 2) << std::endl;
-
-		std::cout << "Looking for 42 in the map: ";
-		std::cout << easyfind(m, 42) << std::endl;
-	}
-	catch (std::exception &e)
-	{
-		std::cout << e.what() << std::endl;
-	}
 }
 
 int main()
 {
-
-	vectorTest();
-
-	std::cout << std::endl;
-
-	mapTest();
+	test<std::vector	<int> >("VECTOR");
+	test<std::list		<int> >("LIST");
+	test<std::deque		<int> >("DEQUE");
+	test<std::set		<int> >("SET");
+	test<std::multiset	<int> >("MULTISET");
 
 	return (0);
 }
