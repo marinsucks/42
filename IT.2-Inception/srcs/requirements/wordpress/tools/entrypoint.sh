9499e644a50e83c19b8096b2864088999d232f80
mbecker <mbecker@Inception.42.fr>
2024-11-29 17:35:45 +0100

    mariadb set up successfully with wordpress. need now to access wordpress content with nginx, 404 error at the moment. unknown misconfiguration see ya

--- IT.2-Inception/srcs/requirements/wordpress/tools/entrypoint.sh
+++ IT.2-Inception/srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,15 +1,38 @@
-#create a new wordpress configuration file
-wp config create	--allow-root \
-					--dbname=$DB_NAME \
-					--dbuser=$DB_ADMIN_UNAME \
-					--dbpass=$DB_ADMIN_PWD \
-					--dbhost=mariadb:3306 \
-					--path='/var/www/wordpress'
+#!/bin/bash
+
+# start the mariadb service
+SECONDS=0
+while ! mysqladmin ping -h mariadb --silent; do
+	echo "Waiting for MariaDB to start... ${SECONDS} seconds elapsed"
+	sleep 1
+	SECONDS=$((SECONDS+1))
+done
+
+# create a new wordpress configuration file
+/wp-cli.phar config create	--allow-root \
+							--dbname="$DB_NAME" \
+							--dbuser="$ADMIN_NAME" \
+							--dbpass="$ADMIN_PWD" \
+							--dbhost=mariadb:3306 \
+							--path='/var/www/wordpress'
+
+# install wordpress
+/wp-cli.phar core install --allow-root \
+                          --url="$WP_URL" \
+                          --title="$WP_TITLE" \
+                          --admin_user="$ADMIN_NAME" \
+                          --admin_password="$ADMIN_PWD" \
+                          --admin_email="$ADMIN_EMAIL" \
+                          --path='/var/www/wordpress'
 
 # create a new wordpress user
-wp user create	--allow-root \
-				XavierNiel \
-				XavierNiel@example.com \
-				--user_pass='42_4ever_<3' \
-				--role=author \
-				--path='/var/www/wordpress'
+/wp-cli.phar user create	--allow-root \
+							"$USER_NAME" \
+							"$USER_NAME@example.com" \
+							--user_pass="$USER_PWD" \
+							--role=author \
+							--path='/var/www/wordpress'
+
+# start the php-fpm service
+mkdir -p /run/php
+exec /usr/sbin/php-fpm7.4 -F
