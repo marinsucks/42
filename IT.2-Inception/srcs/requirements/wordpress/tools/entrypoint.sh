88a10ef627b0d0fb2270a8254a4a3ad206d1712b
mbecker <mbecker@Inception.42.fr>
2024-12-04 15:09:46 +0100

    website works !! checks needed but in good shape hehe

--- IT.2-Inception/srcs/requirements/wordpress/tools/entrypoint.sh
+++ IT.2-Inception/srcs/requirements/wordpress/tools/entrypoint.sh
@@ -8,38 +8,29 @@ while ! mysqladmin ping -h mariadb --silent; do
 done
 
 # create a new wordpress configuration file
-/wp-cli.phar config create	--allow-root \
-							--dbname="$DB_NAME" \
-							--dbuser="$ADMIN_NAME" \
-							--dbpass="$ADMIN_PWD" \
-							--dbhost=mariadb \
-							--path=/var/www/wordpress
-
-## install wordpress
-#/wp-cli.phar core install --allow-root \
-#                          --url=https://mbecker.42.fr/ \
-#                          --title=Inception \
-#                          --admin_user=admin \
-#                          --admin_password=admin \
-#                          --admin_email=admin@42.fr \
-#                          --path=/var/www/wordpress
+wp config create	--allow-root \
+					--dbname="$DB_NAME" \
+					--dbuser="$ADMIN_NAME" \
+					--dbpass="$ADMIN_PWD" \
+					--dbhost=mariadb \
+					--path=/var/www/wordpress
 
 # install wordpress
-/wp-cli.phar core install --allow-root \
-                          --url=https://$DOMAIN/ \
-                          --title=$WP_TITLE \
-                          --admin_user=$ADMIN_NAME \
-                          --admin_password=$ADMIN_PWD \
-                          --admin_email=$ADMIN_EMAIL \
-                          --path=/var/www/wordpress
+wp core install --allow-root \
+				--url=https://$DOMAIN/ \
+				--title=$WP_TITLE \
+				--admin_user=$ADMIN_NAME \
+				--admin_password=$ADMIN_PWD \
+				--admin_email=$ADMIN_EMAIL \
+				--path=/var/www/wordpress
 
 # create a new user
-/wp-cli.phar user create	--allow-root \
-							"$USER_NAME" \
-							"$USER_NAME@example.com" \
-							--user_pass="$USER_PWD" \
-							--role=author \
-							--path=/var/www/wordpress
+wp user create	--allow-root \
+				"$USER_NAME" \
+				"$USER_NAME@example.com" \
+				--user_pass="$USER_PWD" \
+				--role=author \
+				--path=/var/www/wordpress
 
 
 
