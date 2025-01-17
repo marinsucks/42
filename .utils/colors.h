12a16dd8ea2f1585e14c4daa0c92c0b5440ea7a4
Marin Becker <mbecker@student.42.fr>
2025-01-17 17:57:35 +0100

    updated webserv to latest `main` version.

--- .utils/colors.h
+++ .utils/colors.h
@@ -6,25 +6,28 @@
 /*   By: mbecker <mbecker@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/09 15:28:39 by mbecker           #+#    #+#             */
-/*   Updated: 2025/01/16 12:15:53 by mbecker          ###   ########.fr       */
+/*   Updated: 2025/01/16 16:13:43 by mbecker          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef	COLORS_H
 # define COLORS_H
 
-# define RED "\033[1;31m"
-# define LRED "\033[0;31m"
-# define GREEN "\033[1;32m"
-# define LGREEN "\033[0;32m"
-# define YELLOW "\033[1;33m"
-# define LYELLOW "\033[0;33m"
-# define BLUE "\033[1;34m"
-# define LBLUE "\033[0;34m"
-# define MAGENTA "\033[1;35m"
-# define LMAGENTA "\033[0;35m"
-# define CYAN "\033[1;36m"
-# define LCYAN "\033[0;36m"
+//! B = Bold, NC = No Color
+
+# define RED "\033[0;31m"
+# define BRED "\033[1;31m"
+# define GREEN "\033[0;32m"
+# define BGREEN "\033[1;32m"
+# define YELLOW "\033[0;33m"
+# define BYELLOW "\033[1;33m"
+# define BLUE "\033[0;34m"
+# define BBLUE "\033[1;34m"
+# define MAGENTA "\033[0;35m"
+# define BMAGENTA "\033[1;35m"
+# define CYAN "\033[0;36m"
+# define BCYAN "\033[1;36m"
+# define GREY "\033[0;90m"
 # define NC "\033[0m"
 
 #endif
